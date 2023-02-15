4ed0c8f27f15acaaa0dc383a88f47074af9ce8a0
HARSHAVARDHANM-603 <hmokala@futurensetech.com>
2023-02-15 15:30:29 +0530

    ratings python

---
+++ assignments/hadoop/mr/ratings.py
@@ -0,0 +1,13 @@
+from mrjob.job import MRJob
+
+
+class Rating_count(MRJob):
+        def mapper(self, _, line):
+                (userID,movieID, rating, timestamp) = line.split(',')
+                yield(rating, 1)
+
+        def reducer(self, rate, counts):
+                yield(rate, sum(counts))
+
+if __name__ == '_main_':
+	Rating_count.run()
