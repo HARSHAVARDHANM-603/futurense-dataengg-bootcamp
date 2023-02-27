c52f167ef734dcbc41aba7ddce2639a28a3910eb
HARSHAVARDHANM-603 <hmokala@futurensetech.com>
2023-02-27 15:22:57 +0530

    ASK

--- QUESTION4.py
+++ QUESTION4.py
@@ -2,13 +2,10 @@ from mrjob.job import MRJob
 
 class Count(MRJob):
         def mapper(self, _, line):
-              for word in line.split():
-                    yield(word, 1)
+                for word in line.split():
+                        yield(word, 1)           
         def reducer(self, word, counts):
-              ans = dict()
-              ans[word] = sum(counts)
-              yield ans
+                yield(word, sum(counts))
 
 if __name__ == '__main__':
         Count.run()
-
